b7455d5e137e29c77985ba3dae322c8d8a157094
Rohan <voidrohan@gmail.com>
2019-04-17 14:37:08 +0530

    first commit

---
+++ play.js
@@ -0,0 +1,16 @@
+const name  = 'max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser =  
+
+function summarizeUser(userName, userAge, userHasHobby)
+{
+    return('Name  is ' +  userName  + ', age is ' +  userAge + ' and the user has hobbies: ' + userHasHobby);
+}
+
+
+
+console.log(summarizeUser(name, age, hasHobbies));
